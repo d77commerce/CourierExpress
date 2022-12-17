61b59e5baa8d85adb5507a7f53276c07b0217f60
d77commerce <d77commerce@hotmail.com>
2022-12-17 14:07:58 +0000

    update

--- CourierExpress/Program.cs
+++ CourierExpress/Program.cs
@@ -6,15 +6,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.EnableSensitiveDataLogging();
     options.UseSqlServer(connectionString);
 });
-   
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<AppUser>(options =>
@@ -27,15 +24,16 @@ builder.Services.AddDefaultIdentity<AppUser>(options =>
     })
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews().AddMvcOptions(options =>
+builder.Services.AddControllersWithViews()
+    .AddMvcOptions(options =>
 {
     options.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
 });
-builder.Services.AddResponseCaching(); 
+builder.Services.AddResponseCaching();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -43,7 +41,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
